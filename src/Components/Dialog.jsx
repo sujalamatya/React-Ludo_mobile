7575065053a97f56687c5300b535495290908dc5
sujal amatya <sujalcr10@gmail.com>
2024-12-20 18:56:24 +0545

    example

--- src/Components/Dialog.jsx
+++ src/Components/Dialog.jsx
@@ -11,7 +11,7 @@ const DialogDemo = () => {
         <Dialog.Overlay className="fixed inset-0 bg-blackA6 data-[state=open]:animate-overlayShow" />
         <Dialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[450px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
           <Dialog.Title className="m-0 text-[17px] font-medium text-mauve12">
-            Waiting...
+            Waiting...xyZ
           </Dialog.Title>
           <Dialog.Description className="mb-5 mt-2.5 text-[15px] leading-normal text-mauve11">
             Waiting for players...
